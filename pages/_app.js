e35cfa3861b98293ae8090949ff7fe0224494e33
Taylor Gorman <me@taylorpatrickgorman.com>
2022-10-26 12:14:00 -0500

    Integrate Vercel analytics
    According to https://vercel.com/docs/concepts/analytics/audiences/quickstart

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
+import { Analytics } from '@vercel/analytics/react'
 import '../styles/normalize.css'
 import '../styles/styles.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return ( <>
+    <Component {...pageProps} />
+    <Analytics />
+  </> )
 }
 
 export default MyApp
